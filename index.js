4d64aaa29fb1a1ad8d8d8e06b9c36800958bf309
IrinaKolganova <irene1805@yandex.ru>
2023-04-01 00:50:49 +0300

    star task is done

---
+++ index.js
@@ -0,0 +1,7 @@
+task = document.querySelector('#task');
+addBtn = document.querySelector('#addBtn');
+toDo = document.querySelector('#toDo');
+clearBtn = document.querySelector('#clearBtn');
+
+// textInput.addEventListener('keyup', (e) => {
+//     textView.innerHTML = e.target.value;})
